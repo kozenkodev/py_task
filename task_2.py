61fe0781879b95dab005be1f28187fa91e2048eb
kozenkodev <65870072+kozenkodev@users.noreply.github.com>
2020-07-22 10:58:01 +0300

    Create task_2.py
    remove from file B all similar numbers with A

---
+++ task_2.py
@@ -0,0 +1,23 @@
+import re
+base_storage=[]
+file_A=[string.strip('\n') for string in open("А.txt",mode="r",encoding="utf-8").readlines()]
+file_B=[string.strip('\n') for string in open("Б.txt",mode="r",encoding="utf-8").readlines()]
+for number in file_A:
+    for string in file_B:
+        if str(re.findall(r'\d+',string)).__contains__(number) and string not in base_storage:
+            print(string)
+            print(number)
+            base_storage.append(string)
+if len(base_storage)>0:
+    print("Было найдено следующие строки для вырезки:")
+    print(base_storage)
+    print("Они вырезаны из файла Б")
+    file_save=open("Б.txt",mode="w",encoding="utf-8")
+    for string in file_B:
+            if string not in base_storage:
+             file_save.write(string+"\n")
+    file_save.close()
+else:
+    print("Повторений не было найдено!")
+
+
